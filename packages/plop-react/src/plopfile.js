d130ad295ebcf7ac57c909a4440589caa653c243
Alexandros Karagiannis <pp.alexkara@gmail.com>
2024-02-15 08:58:24 -0700

    fix(config): update config fetching

--- packages/plop-react/src/plopfile.js
+++ packages/plop-react/src/plopfile.js
@@ -1,5 +1,5 @@
 import merge from "lodash.merge";
-import { defaultConfig } from './generators/config/default.js'
+import { defaultConfig } from "./generators/config/default.js";
 import { prompts as componentPrompts } from "./generators/component/prompts.js";
 import { actions as componentActions } from "./generators/component/actions.js";
 import { prompts as genericPrompts } from "./generators/generic/prompts.js";
@@ -11,42 +11,43 @@ import { helpers } from "./helpers/helpers.js";
 import { getConfig } from "./utils/getConfig.js";
 
 export default async function (plop) {
-  console.log('-------------React Code Generation-------------')
+  console.log("-------------React Code Generation-------------");
   let externalConfig;
 
   try {
-    externalConfig = getConfig()
+    externalConfig = await getConfig();
     const { isValid, validationErrors } = validateConfig(externalConfig);
-    
+
     if (!isValid) {
       throw new Error(validationErrors);
     }
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-    const combinedConfig = merge(defaultConfig,externalConfig)
-    const genPrompts = genericPrompts(combinedConfig);
-    const compPrompts = componentPrompts(combinedConfig);
-    const cntxPrompts = contextPrompts(combinedConfig);
-    const fnPrompts = functionPrompts(combinedConfig);
-    const hkPrompts = hookPrompts(combinedConfig);
-    const combinedPrompts = [
-      ...genPrompts,
-      ...compPrompts,
-      ...cntxPrompts,
-      ...fnPrompts,
-      ...hkPrompts,
-    ];
-    helpers(plop);
-    plop.setGenerator("react", {
-      description: "Complete React Creation",
-      prompts: combinedPrompts,
-      actions: (data) => {
-        if (data.category === "Component") {
-          return componentActions(data, combinedConfig);
-        }
-        return [];
-      },
-    });
 
+  const combinedConfig = merge(defaultConfig, externalConfig);
+
+  const genPrompts = genericPrompts(combinedConfig);
+  const compPrompts = componentPrompts(combinedConfig);
+  const cntxPrompts = contextPrompts(combinedConfig);
+  const fnPrompts = functionPrompts(combinedConfig);
+  const hkPrompts = hookPrompts(combinedConfig);
+  const combinedPrompts = [
+    ...genPrompts,
+    ...compPrompts,
+    ...cntxPrompts,
+    ...fnPrompts,
+    ...hkPrompts,
+  ];
+  helpers(plop);
+  plop.setGenerator("react", {
+    description: "Complete React Creation",
+    prompts: combinedPrompts,
+    actions: (data) => {
+      if (data.category === "Component") {
+        return componentActions(data, combinedConfig);
+      }
+      return [];
+    },
+  });
 }
